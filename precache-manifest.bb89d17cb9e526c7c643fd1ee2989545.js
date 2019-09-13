cc9227171e757909cba30c1eb14fd634278014ce
zuaam <46837548+zuaam@users.noreply.github.com>
2019-09-13 09:13:42 -0700

    Updates

---
+++ precache-manifest.bb89d17cb9e526c7c643fd1ee2989545.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5190509d9cadf8d9808c8ceb4cd67cd3",
+    "url": "/react-web-application/index.html"
+  },
+  {
+    "revision": "10b4ac0f16f8b2e8342e",
+    "url": "/react-web-application/static/css/main.8244ae29.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/react-web-application/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "10b4ac0f16f8b2e8342e",
+    "url": "/react-web-application/static/js/main.c35f041c.chunk.js"
+  },
+  {
+    "revision": "6dce8ffc9e2faf4244c1",
+    "url": "/react-web-application/static/js/runtime~main.e486990c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-web-application/precache-manifest.bb89d17cb9e526c7c643fd1ee2989545.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-web-application/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
